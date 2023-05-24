5642b9e4f305fec30ae529cfc77205cc5c7126db
Sagar Paresh Shah <sg355741@dal.ca>
2023-05-23 23:04:34 -0300

    test commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./assets/cobblr-dark-logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <div>This commit is made by Sagar. Happy coding!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
