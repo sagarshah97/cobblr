954d8ccfce891d8fb3105f1eb5cd66955be254d2
Aayush Yogesh Pandya <ay923755@dal.ca>
2023-05-24 15:25:17 -0300

    init commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" />
+        <div>Hello World! - Aayush</div>
         <div>Welcome to the world of kicks. -Pratik</div>
         <div>This commit is made by Sagar. Happy coding!</div>
       </header>
