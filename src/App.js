9d41bbcadf96be12d94a7dbfe3f51003afe198c2
ASHISH OJHA <as950640@dal.ca>
2023-05-24 15:49:43 -0300

    Test Commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" />
+        <div>Test Commit - Ashish</div>
         <div>Hello World! - Aayush</div>
         <div>Welcome to the world of kicks. -Pratik</div>
         <div>This commit is made by Sagar. Happy coding!</div>
