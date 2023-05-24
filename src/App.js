1a67d9f60173cea5461020f45b5ad87151868f40
Pratik <pratikparmar@dal.ca>
2023-05-24 00:36:19 -0300

    second initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" />
-        <div>Welcome to the world of kicks. -P</div>
+        <div>Welcome to the world of kicks. -Pratik</div>
         <div>This commit is made by Sagar. Happy coding!</div>
       </header>
     </div>
