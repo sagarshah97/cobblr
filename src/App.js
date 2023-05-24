ecdeb8608a3af84810f2dda3f891290b544f2bae
Jayant Patidar <jayantcrusader@gmail.com>
2023-05-24 18:27:51 -0300

    test commit by jayant

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} alt="logo" />
         <div>Test Commit - Ashish</div>
         <div>Hello World! - Aayush</div>
+        <div>Hey there! - Jayant</div>
         <div>Welcome to the world of kicks. -Pratik</div>
         <div>This commit is made by Sagar. Happy coding!</div>
         <div>Hello Sneaker Lovers! welcome back - Sahil</div>
