adf806ef7199c7af54dcb0ef3cd0b74f1e8efe55
Sagar Paresh Shah <sg355741@dal.ca>
2023-06-30 13:59:58 -0300

    added API validation to lib

---
+++ backend/app/lib/expressValidation.js
@@ -0,0 +1,13 @@
+const expressValidation = require("express-validation");
+
+class ExpressValidation {
+  constructor() {
+    this.ValidationError = expressValidation.ValidationError;
+  }
+
+  validate(schema) {
+    return expressValidation.validate(schema, {}, { abortEarly: false });
+  }
+}
+
+module.exports = new ExpressValidation();
