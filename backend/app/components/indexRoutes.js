c77591bc9ba076da65bfb554f9d2a0d6bbd00f3e
Aayush Yogesh Pandya <ay923755@dal.ca>
2023-07-10 09:28:58 -0300

    Wishlist API (get, add, remove)

--- backend/app/components/indexRoutes.js
+++ backend/app/components/indexRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const shoesRoutes = require("./shoes/shoes.route");
+const adminRoutes = require("./admin/admin.route");
+const wishlistRoutes = require("./wishlist/wishlist.route");
 
 const healthCheck = (request, response) => {
   response.status(200).send({
@@ -9,6 +11,8 @@ const healthCheck = (request, response) => {
 };
 
 router.use("/shoes", shoesRoutes);
+router.use("/admin", adminRoutes);
+router.use("/wishlist", wishlistRoutes);
 
 module.exports.router = router;
 module.exports.healthCheck = healthCheck;
