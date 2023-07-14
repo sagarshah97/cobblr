26044e3377bce57cab9a669cb8b0a2928f28777b
ASHISH OJHA <as950640@dal.ca>
2023-07-14 17:53:04 -0300

    Added api to get order details for order cnf page

--- backend/app/components/indexRoutes.js
+++ backend/app/components/indexRoutes.js
@@ -4,6 +4,7 @@ const shoesRoutes = require("./shoes/shoes.route");
 const faqRoutes = require("./faq/faq.route");
 const userRoutes = require("./users/users.route");
 const contactRoutes = require("./contact/contact.route");
+const orderRoutes = require("./orders/orders.route");
 
 const healthCheck = (request, response) => {
   response.status(200).send({
@@ -15,6 +16,7 @@ router.use("/shoes", shoesRoutes);
 router.use("/faq", faqRoutes);
 router.use("/users", userRoutes);
 router.use("/contact", contactRoutes);
+router.use("/orders", orderRoutes);
 
 module.exports.router = router;
 module.exports.healthCheck = healthCheck;
---
+++ backend/app/components/orders/orders.route.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { validate } = require("../../lib/expressValidation");
+const OrderController = require("./orders.controller");
+const ordersValidation = require("./orders.validation");
+
+const router = express.Router();
+const orderController = new OrderController();
+
+router
+  .route("/getorder")
+  .get(
+    validate(ordersValidation.getOrder),
+    orderController.getOrderById.bind(orderController)
+  );
+module.exports = router;
