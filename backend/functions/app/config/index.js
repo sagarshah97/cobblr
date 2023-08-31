75987d7c414a368cda65d9929f65dc1ddf5f2b62
Sagar Paresh Shah <sg355741@dal.ca>
2023-08-30 22:30:48 -0300

    removed env variables

--- backend/functions/app/config/index.js
+++ backend/functions/app/config/index.js
@@ -1,21 +1,19 @@
 // Author: Sagar Paresh Shah (B00930009)
 
 const HOST = "cobblr";
-const USER = "aayushypandya";
-const PASSWORD = "adminadmin";
+const USER = "";
+const PASSWORD = "";
 const DB = "cobblr";
-const CLUSTER = "csci-5709";
-const TLD = "m6riq79";
+const CLUSTER = "";
+const TLD = "";
 const jwtConfig = {
-  secretKey: "atsd3ygd8236r87qgd9wa8dhq8",
+  secretKey: "",
   timeout: 7200000,
 };
-const stripeApiKey =
-  "sk_test_51NQaRTG6OTDlGgErIzLxYcY7MQ4c2bNYT2CmckA0yRMQcBhyya5wFNDk1ONJWZS2c2dnl49HfPYA8h7TyvIWAaWg00dvrlhtsw";
-const sendGridApiKey =
-  "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";
+const stripeApiKey = "";
+const sendGridApiKey = "";
 const frontendBaseUrl = "https://cobblr-store.netlify.app";
-const cobblrEmail = "cobblr5709@gmail.com";
+const cobblrEmail = "";
 
 module.exports = {
   HOST,
